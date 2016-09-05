c8865d46e068bab57cb2244fbae564b67502ede8
Enrique Zamudio <alterbass@gmail.com>
2016-09-05 11:48:16 -0500

    remove unused code, append action to submit button

--- jsclient/client.js
+++ jsclient/client.js
@@ -3,23 +3,10 @@ require.config({
     waitSeconds: 5
 });
 
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
 require(["ceylon/language/1.2.3/ceylon.language-1.2.3",
     'scriptbowl/chat/client/1.0.0/scriptbowl.chat.client-1.0.0'], function(cl, chat) {
     cl.print(cl.runtime().version);
     //Login
     document.getElementById("logon").onclick=chat.login;
+    document.getElementById("send").onclick=chat.submit;
 });
