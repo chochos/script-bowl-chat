7e9861ea760c8dd94169e75191ebac39380cff27
Enrique Zamudio <alterbass@gmail.com>
2016-09-06 11:52:22 -0500

    1.3.0

--- jsclient/client.js
+++ jsclient/client.js
@@ -3,7 +3,7 @@ require.config({
     waitSeconds: 5
 });
 
-require(["ceylon/language/1.2.3/ceylon.language-1.2.3",
+require(["ceylon/language/1.3.0/ceylon.language-1.3.0",
     'scriptbowl/chat/client/1.0.0/scriptbowl.chat.client-1.0.0'], function(cl, chat) {
     chat.setup();
 });
